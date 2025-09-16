1b39652db801eb8062aa38e1383375318ceb00ef
Josue <jj2022074259@virtual.upt.pe>
2025-09-16 01:58:35 -0500

    Configuracion de la ventana GLUT

--- cad2D/main.cpp
+++ cad2D/main.cpp
@@ -1,34 +1,45 @@
-// main.cpp
 #include <GL/glut.h>
-#include "utilidades/menu.hpp"
-#include "utilidades/eventos.hpp"
 
-// Parámetros iniciales de la ventana
 int ancho_ventana = 800;
 int alto_ventana  = 600;
 
-void inicializarVentana() {
+
+void configurarVentana() {
+    glClearColor(1.0, 1.0, 1.0, 1.0); //Fondo de color blanco
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, ancho_ventana, 0, alto_ventana);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void dibujar() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+void redimensionar(int w, int h) {
+    ancho_ventana = w;
+    alto_ventana  = h;
+    glViewport(0, 0, w, h);
+    configurarVentana();
+    glutPostRedisplay();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(ancho_ventana, alto_ventana);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("CAD 2D - Proyecto Josue");
 
-    glClearColor(1.0, 1.0, 1.0, 1.0); // fondo blanco
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0, ancho_ventana, 0, alto_ventana); // origen abajo-izquierda
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-}
+    configurarVentana();
 
-int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    inicializarVentana();
-
-    eventos::registrarCallbacks();  // mouse, teclado, redibujar
-    menu::crearMenu();              // menú popup con botón derecho
+    glutDisplayFunc(dibujar);
+    glutReshapeFunc(redimensionar);
 
     glutMainLoop();
     return 0;
 }
+
 
